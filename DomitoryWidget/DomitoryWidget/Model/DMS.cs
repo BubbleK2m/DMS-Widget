38abaac9e61b756648ab37b86063f92d158b4286
DSMdongly <gdh0608@naver.com>
2018-06-19 23:50:57 +0900

    Auth 요청 구현

---
+++ DomitoryWidget/DomitoryWidget/Model/DMS.cs
@@ -0,0 +1,37 @@
+﻿using EasyHttp.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomitoryWidget.Model
+{
+    static class DMS
+    {
+        static class URL
+        {
+            public const string BASEPATH = "http://dsm2015.cafe24.com";
+            public const string AUTH = BASEPATH + "/auth";                
+        }
+
+        public static HttpResponse Auth(string id, string password)
+        {
+            var client = new HttpClient();
+
+            var body = new Dictionary<string, object>
+            {
+                { "id", id },
+                { "password", password },
+            };
+
+            var response = client.Post
+            (
+                uri: URL.AUTH, data: body, 
+                contentType: HttpContentTypes.ApplicationJson
+            );
+
+            return response;
+        }
+    }
+}
